72d514d146b5d8cd1d99aaa25d2fa66a87bc10ba
Breather08 <amanbekov.nurbek@gmail.com>
2020-11-18 01:39:53 +0600

    AStar implemented

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type Tile struct {
 	distance     int
 	cost         int
 	CostDistance int
-	Parent       interface{}
+	Parent       *Tile
 }
 
 var maxX = 12
@@ -24,12 +24,13 @@ var maxY = 10
 func (t *Tile) setCostDistance() {
 	t.CostDistance = t.cost + t.distance
 }
-
+// Manhattan
 func (t *Tile) setDistance(targetX, targetY int) {
 	t.distance = int(math.Abs(float64(targetX-t.X)) + math.Abs(float64(targetY-t.Y)))
 }
 
 func getPossibleTiles(gameMap []string, currentTile, targetTile *Tile) []*Tile {
+	// currentTile.setCostDistance()
 	tilesDir := []*Tile{
 		&Tile{X: currentTile.X, Y: currentTile.Y - 1, cost: currentTile.cost + 1, Parent: currentTile},
 		&Tile{X: currentTile.X, Y: currentTile.Y + 1, cost: currentTile.cost + 1, Parent: currentTile},
@@ -60,8 +61,7 @@ func remove(s []*Tile, target *Tile) (res []*Tile) {
 	return
 }
 
-func main() {
-	defer fmt.Println()
+func AStar() {
 	fileMap, err := ioutil.ReadFile("map1.txt")
 	if err != nil {
 		log.Println(err)
@@ -70,7 +70,7 @@ func main() {
 	for _, k := range fileMap {
 		mapStr += string(k)
 	}
-	// fmt.Print(mapStr)
+
 	mapArr := strings.Split(mapStr, "\n")
 
 	start := &Tile{
@@ -83,25 +83,40 @@ func main() {
 		Y: 10,
 	}
 
-	start.setDistance(finish.X, finish.Y)
+	// start.setDistance(finish.X, finish.Y)
 
 	activeTiles := []*Tile{start}
 	visitedTiles := []*Tile{}
 
+	itest := 0
+
 	for len(activeTiles) > 0 {
-
-		for _, k := range activeTiles {
-			fmt.Println(k)
-		}
-
+		itest++
 		slice.Sort(activeTiles, func(i, j int) bool {
 			return activeTiles[i].CostDistance < activeTiles[j].CostDistance
 		})
 
 		var checkTile = activeTiles[0]
-
+		// fmt.Println(checkTile.X, ":", checkTile.Y)
+		
 		if checkTile.X == finish.X && checkTile.Y == finish.Y {
-			fmt.Println("EBAT NAHUI, POLUCHILOS' BLYAT'")
+			var tile = checkTile
+			fmt.Println("Retracing steps backwards...")
+			for true {
+				fmt.Println(tile.X, ":", tile.Y)
+				// if mapArr[tile.Y][tile.X] == '.' || mapArr[tile.Y][tile.X] == ';' {
+					var newMapRow = []rune(mapArr[tile.Y])
+					newMapRow[tile.X] = '*'
+					mapArr[tile.Y] = string(newMapRow)
+				// }
+				tile = tile.Parent
+				if tile == nil {
+					// fmt.Println("Map:")
+					for _,k := range mapArr {
+						fmt.Println(k)
+					}
+				}
+			}
 			return
 		}
 
@@ -111,7 +126,6 @@ func main() {
 		possible := getPossibleTiles(mapArr, checkTile, finish)
 	Loop:
 		for _, walkableTile := range possible {
-
 			for _, k := range visitedTiles {
 				if k.X == walkableTile.X && k.Y == walkableTile.Y {
 					continue Loop
@@ -121,8 +135,10 @@ func main() {
 			for _, k := range activeTiles {
 				if k.X == walkableTile.X && k.Y == walkableTile.Y {
 					existingTile := k
-					if existingTile.CostDistance > checkTile.CostDistance {
+					if existingTile.CostDistance < checkTile.CostDistance {
+						fmt.Println("hi")
 						activeTiles = remove(activeTiles, existingTile)
+						activeTiles = append(activeTiles, walkableTile)
 						continue Loop
 					}
 				}
@@ -131,4 +147,8 @@ func main() {
 		}
 
 	}
+}
+
+func main() {
+	AStar()
 }
